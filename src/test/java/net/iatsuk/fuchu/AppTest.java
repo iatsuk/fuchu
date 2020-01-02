8c4a3f6f0f8a06e124375e061a0e4d0a2aa04921
a.iatsuk <a.iatsuk@corp.mail.ru>
2020-01-03 02:01:52 +0300

    infra: added gradle base

---
+++ src/test/java/net/iatsuk/fuchu/AppTest.java
@@ -0,0 +1,15 @@
+package net.iatsuk.fuchu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class AppTest {
+
+    @Test
+    public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+}
